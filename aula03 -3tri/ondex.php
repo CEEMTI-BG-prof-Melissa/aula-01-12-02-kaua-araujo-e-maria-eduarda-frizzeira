b54da61949369cabee33ef725cdd34bfb76a48f1
DUDA FRIZEIRÃO <maria.eafsilva@aluno.edu.es.gov.br>
2025-09-09 11:57:52 -0300

    Create ondex.php

---
+++ aula03 -3tri/ondex.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title> AULA 02 -PHP </title>
+    <body>
+        <?php
+            $nome1 = "Duda Frizzeira ";
+            $nome2 = "Kauã ";
+
+            $n1 = 10;
+            $n2 = 2;
+
+            $soma =$n1+$n2;
+
+            echo " Meu nome é " .$nome1 . " e minha dupla é " . $nome2;
+            echo "<br>Meu nome é $nome1 e minha dupla é $nome2 ";
+            echo "<br><br> A soma de $n1 com $n2 é igual a $soma.";
+        ?>
+    </body>
+</html>
