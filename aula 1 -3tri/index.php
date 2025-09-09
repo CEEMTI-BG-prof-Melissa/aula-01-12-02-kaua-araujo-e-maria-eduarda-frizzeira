eac77813e72b6b215e5750c4bbe60ae246aef354
DUDA FRIZEIRÃO <maria.eafsilva@aluno.edu.es.gov.br>
2025-09-09 11:56:16 -0300

    Create index.php

---
+++ aula 1 -3tri/index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title> AULA 01 -PHP </title>
+    <body>
+        <?php
+            echo "Olá, Mundo! ";
+            echo "Duda frizzeira e Kauã ";
+        ?>
+    </body>
+</html>
