d26a29f3d61878a3aa97fa889b87a88422b72dd2
DUDA FRIZEIRÃO <maria.eafsilva@aluno.edu.es.gov.br>
2025-09-09 11:57:06 -0300

    Create index.php

---
+++ aula 2-3tri/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title> AULA 02 -PHP </title>
+    <body>
+        <?php
+            $nome1 = "Duda Frizzeira ";
+            $nome2 = "Kauã ";
+            
+            echo "Meu nome é " .$nome1 . "e minha dupla é " . $nome2;
+        ?>
+    </body>
+</html>
