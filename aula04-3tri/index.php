d415c66e25e67b3e7f9211a62361f4f415e4d20a
DUDA FRIZEIRÃO <maria.eafsilva@aluno.edu.es.gov.br>
2025-09-23 11:46:59 -0300

    Create index.php

---
+++ aula04-3tri/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title> AULA 04 -PHP </title>
+    <body>
+        <?php
+            $n1 = $_GET ["a"];
+            $n2 = $_GET ["b"];
+
+            echo "<br><br>A soma de $n1 e $n2 é: ".($n1 + $n2);
+            echo "<br><br>A subtração $n1 e $n2 é: ".($n1 - $n2);
+            echo "<br><br>A multiplicação de $n1 e $n2 é: ".($n1*$n2);
+            echo "<br><br>A divisão de $n1 e $n2 é: ".($n1 / $n2);
+            echo "<br><br>O resto da divisão de $n1 e $n2 é: ".($n1 % $n2);
+        ?>
+    </body>
+</html>
