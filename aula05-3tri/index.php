0ffe71d5939c9f3f1ae66e1e3f65e5f39a05f6ed
DUDA FRIZEIRÃO <maria.eafsilva@aluno.edu.es.gov.br>
2025-09-30 11:37:46 -0300

    Update index.php

--- aula05-3tri/index.php
+++ aula05-3tri/index.php
@@ -10,10 +10,15 @@
             echo "<br><br> O valor absoluto de $n1 é " .abs($n1);
             echo "<br><br> O número $n2 <sup>$n1</sup> é igual a " .pow($n2, $n1);
             echo "<br><br> A raiz quadrada de $n2 é igual a " .sqrt($n2);
-            echo "<br><br> O número arredondado é igual a " .round($n2);
-            echo "<br><br> O número arredondado pra cima é igual a " .ceil($n2);
-            echo "<br><br> O número arredondado pra baixo é igual a " .floor($n2);
+            echo "<br><br> O número arredondado $n2 é igual a " .round($n2);
+            echo "<br><br> O número arredondado $n2 pra cima é igual a " .ceil($n2);
+            echo "<br><br> O número arredondado $n2 pra baixo é igual a " .floor($n2);
 
+            echo "<br><br><br>";
+            echo "A parte inteira de $n2 é igual a " .intval($n2);
+
+           echo "<br><br><br>";
+           echo "Um pirulito custa R$" .number_format($n1, 2);
         ?>
     </body>
 </html>
